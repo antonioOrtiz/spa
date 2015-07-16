2c58efbdcb93ef85dfb78b3b445f38a239d86389
antonio ortiz <17antonio.ortiz@gmail.com>
2015-07-15 20:56:41 -0400

    finished chaper 1!

--- app/js/barb.js
+++ app/js/barb.js
@@ -1,37 +1,55 @@
 (function() {
 
-
     window.addEventListener('load', windowLoadHandler, false);
+
+    var todoListItems = [];
+
+    function redrawList() {
+
+        var i;
+        var list = document.getElementById('todo-list');
+        var l = todoListItems.length;
+        list.innerHTML = "";
+
+        for (i = 0; i < l; i++) {
+
+            var item = document.createElement('li');
+            item.appendChild(
+                document.createTextNode(todoListItems[i]));
+            list.appendChild(item);
+        }
+    }
+    
+    function addToList(item) {
+        todoListItems.push(item);
+        localStorage.setItem('todo-list', JSON.stringify(todoListItems));
+    }
+
+    function reloadList(item){
+
+    	var stored = localStorage.getItem('todo-list');
+    		if(stored){
+    			todoListItems = JSON.parse(stored);
+    		}
+    		redrawList();
+    }
 
 
     function newTodoKeyPressHandler(e) {
         if (e.keyCode === 13) {
-<<<<<<< HEAD
-        	var todo = document.getElementById('new-do');
-        	var list = document.getElementById('todo-list');
-        	list.innerHTML += ("<li>"+ todo + "</li>");
-=======
-            var todoField = document.getElementById('new-todo').value;
-            alert('We have a new Todo! ' + todoField);
-            var list = document.getElementById('todo-list'), 
-            item = document.createElement('li');
-            item.appendChild(item);
-            item.appendChild(document.createTextNode(todoField.value));
-            todoField.value = '';
-
-            //list.innerHTML += ("<li>" + todo + "</li>");
->>>>>>> refs/remotes/origin/master
-
+            var todoField = document.getElementById('new-todo');
+            addToList(todoField.value);
+            redrawList();
+            todoField.value = "";
+            /* list.innerHTML += ('<li>' + todo + '</li>'); */
         }
     }
 
-    function windowLoadHandler() {
-        document.getElementById('new-todo').addEventListener(
-            'keypress',
-            newTodoKeyPressHandler,
-            false
-        );
-    }
-
+     window.addEventListener('load', windowLoadHandler, false);
+        function windowLoadHandler() {
+          reloadList();
+          document.getElementById('new-todo').addEventListener('keypress', newTodoKeyPressHandler, false);
+        }
 
 }());
+
